a64805a5f3fb81487c5852f2c62948582433ea2c
consulnn <116591057+consulnn@users.noreply.github.com>
2023-01-31 18:48:35 -0500

    Skeleton from CO script
    Initiation of modules, time track

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
-
+import math
+import os
+import sys
+#from hapi2 import *
+from hapi import *
 import numpy as np
+import matplotlib.pyplot as plt
+sys.path.insert(1,'/work/WORK/WORK/scripts/regular_scripts')
+sys.path.insert(2,'/work/WORK/WORK/scripts/hapi2')
+import json
+from getpass import getpass
+
+import pylab as pl
+import matplotlib as mpl
+import time
+import h5py
+
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Helvetica"]})
+## for Palatino and other serif fonts use:
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Palatino"],
+})
+
+if not os.path.exists("./images"):
+    os.mkdir("./images")
+
+if not os.path.exists("./datafiles"):
+    os.mkdir("./datafiles")
+
+t_begin = time.time()
 
 
 
 
+
+
+t_end = time.time()
+
+print('Time taken: %d seconds'%(t_end-t_begin))
+print('Done.')
 
 
 
