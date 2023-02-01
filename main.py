b88eb4cb27355b6c7f615f2f3a7420d6d8cb588b
consulnn <116591057+consulnn@users.noreply.github.com>
2023-02-01 14:57:59 -0500

    0.1.1
    Finished reading file with parameters

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ import pylab as pl
 import matplotlib as mpl
 import time
 import h5py
+#############################################################
+# TO DO #####################################################
+#############################################################
+# FULL NUMBER OF PARAMS #####################################
+#############################################################
+
+
 
 
 plt.rcParams.update({
@@ -30,15 +37,9 @@ plt.rcParams.update({
     "font.serif": ["Palatino"],
 })
 
-XMASSSEC_VERSION = '0.0.1'; __version__ = XMASSSEC_VERSION
-XMASSSEC_HISTORY = [
-'INITIATION OF INPUT FILE WITH PARAMETERS 31.01.23 (ver. 0.1)'
-]
-
-# version header
-print('X-MASS-SEC version: %s'%(XMASSSEC_VERSION))
-
-INPUT_FILENAME = 'params.inp'
+#############################################################
+### FUNCTIONS ###############################################
+#############################################################
 
 # opens file with necessary parameters for calculations
 def openParametersFile(fname):
@@ -49,29 +50,19 @@ def openParametersFile(fname):
             # input as list of lines in 'lines'
             lines = finp.readlines()
             # check if input has enough parameters
-            if (len(lines)<6):
+            if (len(lines)<17):
                 raise ParamsError
             # removing '\n'
             lines = [item.rstrip('\n') for item in lines]
             # splitting names and values
             params = [item.split(':') for item in lines]
-            print(params)
+            if (FLAG_DEBUG_PRINT):
+                [print(item[0],'\t',item[1]) for item in params]
     except FileNotFoundError:
         print('%s file is not found!'%fname)
     except ParamsError:
         print("Not enought parameters in %s!"%fname)
-    return
-
-
-openParametersFile(INPUT_FILENAME)
-
-
-
-#############################################################
-# TO DO #####################################################
-#############################################################
-# FULL NUMBER OF PARAMS #####################################
-#############################################################
+    return params
 
 
 
@@ -82,14 +73,8 @@ openParametersFile(INPUT_FILENAME)
 
 
 
-
-
-
-
-
-
-
-
+# flag to print values in functions to debug
+FLAG_DEBUG_PRINT = True
 
 if not os.path.exists("./images"):
     os.mkdir("./images")
@@ -97,16 +82,50 @@ if not os.path.exists("./images"):
 if not os.path.exists("./datafiles"):
     os.mkdir("./datafiles")
 
+#############################################################
+### BEGIN OF MAIN PART ######################################
+#############################################################
+
+print("Timer started")
 t_begin = time.time()
 
+XMASSSEC_VERSION = '0.1.1'; __version__ = XMASSSEC_VERSION
+XMASSSEC_HISTORY = [
+'INITIATION OF INPUT FILE WITH PARAMETERS 31.01.23 (ver. 0.1)'
+]
+
+# version header
+print('X-MASS-SEC version: %s'%(XMASSSEC_VERSION))
+
+INPUT_FILENAME = 'params.inp'
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ParametersCalculation = openParametersFile(INPUT_FILENAME)
 
 
 
 t_end = time.time()
-
 print('Time taken: %d seconds'%(t_end-t_begin))
+
 print('Done.')
 
 
