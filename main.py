1caa41fcab205d26e7058b62252b6f0d9a8e8621
consulnn <116591057+consulnn@users.noreply.github.com>
2023-02-09 17:26:09 -0500

    0.2.4
    Output feature
    VMS, Wn function
    Attributes

--- main.py
+++ main.py
@@ -114,12 +114,50 @@ def openTemp(fname,Np):
         print('Corrupted relations between Np and NpxNt array')
         sys.exit()
 
+# opens pressure array file
+def openVMS(fname):
+    try:
+        avms = np.genfromtxt(fname,dtype='float')
+        global FLAG_DEBUG_PRINT
+        if (FLAG_DEBUG_PRINT):
+            print('*** DEBUG: VMS input ***')
+            [print('%12.6f'%(item)) for item in avms]
+            print('*** END: VMS input ***\n')
+        print('*********\nVMS file %s is opened well\nTotal number of lines: %d\n*********'%(fname,len(avms)))
+        return avms, len(avms)
+    except FileNotFoundError:
+        print('%s file is not found!'%fname)
+        sys.exit()
+
+def openXgenetareWn(fname,params):
+    try:
+        with open(fname) as f:
+            Nwn = int(f.readline())
+            wn_begin = float(params[3][1])
+            wn_end = float(params[4][1])
+            WN_range = np.linspace(wn_begin,wn_end,Nwn)
+#             print(WN_range[:15])
+            if (FLAG_DEBUG_PRINT):
+                print('*** DEBUG: WN input ***')
+                [print('%12.6f'%item1) for item1 in WN_range[:10]]
+                print('...')
+                [print('%12.6f'%item2) for item2 in WN_range[-10:]]
+                print(Nwn)
+                print('*** END: WN input ***\n')
+            print('*********\nWN file %s is opened well\nTotal number of wn-points: %d\n*********'%(fname,Nwn))
+                
+            return WN_range, Nwn
+    except FileNotFoundError:
+        print('%s file is not found!'%fname)
+        sys.exit()
+
 
 # creates a file HDF5 and saturate it with attributes
-def OpenHDF5(fname,params,pres, temp, Np, Nt):
+def OpenHDF5(fname,params,pres, temp, vms, Np, Nt, Nvms):
     global FLAG_DEBUG_PRINT
     INDEX_TEMP = '%02d'%(5)
-    print(INDEX_TEMP)
+    INDEX_Qbrd_TEMP = '%02d'%(1)
+#    print(INDEX_TEMP)
     try:
         f = h5py.File(fname, mode='w')
         global FLAG_OPENED_HDF5 
@@ -135,10 +173,14 @@ def OpenHDF5(fname,params,pres, temp, Np, Nt):
             [print(item, f.attrs[item]) for item in f.attrs.keys()]
             print('*** END: Attributes ***\n')
         # f.create_dataset('Gas_05_Absorption',shape=)
-        f.create_dataset('Gas_Index',data=INDEX_TEMP)
-        f.create_dataset('Pressure',data=pres)
-        f.create_dataset('Temperature',data=temp)
-        print(f.keys())
+
+        ds_index = f.create_dataset('Gas_Index',data=INDEX_TEMP)
+        ds_pres = f.create_dataset('Pressure',data=pres)
+        ds_temp = f.create_dataset('Temperature',data=temp)
+        ds_vms = f.create_dataset('Broadener_01_VMS',data=vms)
+        ds_vms.attrs.__setitem__('broadener_name','h2o')
+        ds_Qbrd = f.create_dataset('Broadener_Index',data=INDEX_Qbrd_TEMP)
+#        print(ds_pres[()])
         return f
     except FileExistsError:
         print('Attempt to re-write file!')
@@ -186,8 +228,10 @@ def CalculateXsec():
 
 
 
-# flag to print values in functions to debug
+# flag to print values in functions to debug 
 FLAG_DEBUG_PRINT = True
+# flag to store prints in OUTPUT.LOG file
+FLAG_LOG_FILE = True
 # flag to open\close HDF5 file 
 FLAG_OPENED_HDF5 = False
 
@@ -205,18 +249,30 @@ if not os.path.exists("./datafiles"):
 print("Timer started")
 t_begin = time.time()
 
-XMASSSEC_VERSION = '0.2.3'; __version__ = XMASSSEC_VERSION
+XMASSSEC_VERSION = '0.2.4'; __version__ = XMASSSEC_VERSION
 XMASSSEC_HISTORY = [
 'INITIATION OF INPUT FILE WITH PARAMETERS 31.01.23 (ver. 0.1)',
 'CREATION OF HDF5 FILE + SOME EXCEPTIONS HANDLING (ver. 0.2)',
 'CLOSING HDF5 FILE AND SATURATION OF ATTRIBUTES (ver. 0.2.1)',
 'SATURATION OF ATTRIBUTE (ROOT) (ver. 0.2.2)',
 'INPUTS FOR P,T AND SATURATION OF ATTRUBUTES (ver. 0.2.3)',
-'INPUTS FOR VMS AND SATURATION OF ATTRUBUTES (ver. 0.2.x)'
+'INPUTS FOR VMS, WN AND SATURATION OF ATTRUBUTES + OUTPUT LOG FILE (ver. 0.2.4)'
 ]
 
 # version header
 print('X-MASS-SEC version: %s'%(XMASSSEC_VERSION))
+
+#####################################################
+
+if (FLAG_LOG_FILE):
+    orig_stdout = sys.stdout
+    fLog = open('output.log', 'w')
+    sys.stdout = fLog
+else:
+    fLog = open('output.log', 'w')
+    fLog.close()
+
+
 
 INPUT_FILENAME = 'params.inp'
 
@@ -224,16 +280,19 @@ PRES_FILENAME = 'pres.inp'
 
 TEMP_FILENAME = 'temps.inp'
 
+VMS_FILENAME = 'vms.inp'
+
+WN_FILENAME = 'wn.inp'
+
 ParametersCalculation = openParametersFile(INPUT_FILENAME)
 
 Pressures, Np = openPressure(PRES_FILENAME)
 
 (Temps, Npp,Ntt) = openTemp(TEMP_FILENAME, Np)
 
+(VMSs, Nvms) = openVMS(VMS_FILENAME)
 
-
-
-
+WNs, Nwn = openXgenetareWn(WN_FILENAME,ParametersCalculation)
 
 db_begin('05_hit20')
 
@@ -241,7 +300,11 @@ print(tableList())
 
 HDF5FileName = 'CO_HDF5.hdf5'
 
-co_hdf5 = OpenHDF5(HDF5FileName, ParametersCalculation, Pressures, Temps, Npp, Ntt)
+co_hdf5 = OpenHDF5(HDF5FileName, ParametersCalculation, Pressures, Temps, VMSs, Npp, Ntt, Nvms)
+
+#print(co_hdf5['Broadener_01_VMS'].attrs['broadener_name'])
+
+
 
 CalculateXsec()
 
@@ -293,6 +356,9 @@ CloseHDF5(co_hdf5)
 
 
 
+if (FLAG_LOG_FILE):
+    sys.stdout = orig_stdout
+    fLog.close()
 
 
 
